596d021b6ea3f3e4eeb2c46db60ec25f2f2ec7fc
Christopher Folmar <c.folmar@outlook.com>
2018-05-26 23:28:00 -0400

    Initial Commit

---
+++ IPVerifier-React/Scripts/React/webpack.config.js
@@ -0,0 +1,26 @@
+﻿var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    context: __dirname,
+    entry: './app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env', 'babel-preset-react']
+                    }
+                }
+            }
+        ]
+    }
+};
