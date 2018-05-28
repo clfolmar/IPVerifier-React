2fbadf107f6e3121674dce82c3e37b33cf09b674
Christopher Folmar <c.folmar@outlook.com>
2018-05-28 03:09:28 -0400

    Test Work

---
+++ IPVerifier-React/Scripts/React/Container.js
@@ -0,0 +1,89 @@
+﻿import React, { PropTypes as T } from 'react'
+import ReactDOM from 'react-dom'
+import { GoogleApiWrapper } from 'google-maps-react';
+
+export const Container = React.createClass({
+    propTypes: {
+        children: T.element.isRequired,
+        auth: T.instanceOf(AuthService)
+    },
+    ///////////////////////////////////////////////////////
+    contextTypes: {
+        router: T.object
+    },
+    ///////////////////////////////////////////////////////
+    getInitialState() {
+        return {
+            places: [],
+            pagination: null,
+            profile: this.props.route.auth.getProfile()
+        }
+    },
+    ///////////////////////////////////////////////////////
+    componentDidUpdate(prevProps) {
+        profile: this.props.route.auth.getProfile()
+    },
+    logout() {
+        this.props.route.auth.logout()
+        this.context.router.push('/about');
+    },
+    ///////////////////////////////////////////////////////
+    renderChildren: function () {
+
+        const { children } = this.props;
+        if (!children) return;
+        const sharedProps = {
+            google: this.props.google,
+            loaded: this.props.loaded,
+            auth: this.props.route.auth
+        }
+        return React.Children.map(children, c => {
+            return React.cloneElement(c, sharedProps, {
+            });
+        })
+    },
+    ///////////////////////////////////////////////////////
+    render: function () {
+        const { routeMap, routeDef } = this.props;
+        const { router } = this.context;
+        const props = this.props;
+        const c = this.renderChildren();
+
+        return (
+            <div className={styles.container}>
+                <div className={styles.topbar}>
+                    {this.props.route.auth.loggedIn() ? (
+                        [
+                            <Link className={styles.topbarItem} to="/">PadStats</Link>,
+                            <Link className={styles.topbarItem} to="/Header">Search</Link>,
+                            <Link className={styles.topbarItem} to="/Home">My Searches</Link>,
+                            <a className={styles.topbarItem} onClick={this.logout.bind(this)}>Logout</a>
+                        ]
+                    ) : (
+                            [
+                                <Link className={styles.topbarItemRight} to="/">PadStats</Link>,
+                                <a className={styles.topbarItemLeft} onClick={this.props.route.auth.login.bind(this)}>Login/Register</a>
+                            ]
+                        )}
+
+                </div>
+                {/*  Create log in buttons and button for main map. */}
+                <div className={styles.wrapper}>
+                    <div id="content" className={styles.content}>
+                        {/*  <Header />*/}
+                        <Map google={google}
+                            className={'map'}
+                            visible={false}>
+                            {c}
+                        </Map>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+})
+
+export default GoogleApiWrapper({
+    apiKey: (process.env.GAPI_KEY || __GAPI_KEY__)
+})(Container)
--- IPVerifier-React/Scripts/React/app.js
+++ IPVerifier-React/Scripts/React/app.js
@@ -7,25 +7,111 @@ import {
     GoogleMap,
     Marker
 } from "react-google-maps";
-
-const MyMapComponent = compose(
-    withProps({
-        googleMapURL:
-        "https://maps.googleapis.com/maps/api/js?key=AIzaSyCY86Q1_a7yhfX-yWBxsi_rgkKtTsELUGk&v=3.exp&libraries=geometry,drawing,places",
-        loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `400px` }} />,
-        mapElement: <div style={{ height: `100%` }} />
-    }),
-    withScriptjs,
-    withGoogleMap
-)(props => (
-    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 48.8582, lng: 2.3387000000000002 }}>
-        {props.isMarkerShown && (
-            <Marker position={{ lat: 48.8582, lng: 2.3387000000000002 }} />
-        )}
-    </GoogleMap>
-));
-
-ReactDOM.render(<MyMapComponent isMarkerShown />, document.getElementById("root"));
+import { GoogleApiWrapper } from 'google-maps-react';
 
 
+//const MyMapComponent = compose(
+//    withProps({
+//        googleMapURL:
+//        "https://maps.googleapis.com/maps/api/js?key=AIzaSyCY86Q1_a7yhfX-yWBxsi_rgkKtTsELUGk&v=3.exp&libraries=geometry,drawing,places",
+//        loadingElement: <div style={{ height: `100%` }} />,
+//        containerElement: <div style={{ height: `400px` }} />,
+//        mapElement: <div style={{ height: `100%` }} />
+//    }),
+//    withScriptjs,
+//    withGoogleMap
+//)(props => (
+//    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 48.8582, lng: 2.3387000000000002 }}>
+//        {props.isMarkerShown && (
+//            <Marker position={{ lat: 48.8582, lng: 2.3387000000000002 }} />
+//        )}
+//    </GoogleMap>
+//));
+
+class Container extends React.Component {
+    render() {
+        const style = {
+            width: '100vw',
+            height: '80vh'
+        }
+        if (!this.props.loaded) {
+            return <div>Loading...</div>
+        }
+        return (
+            <div style={style}>
+                <span>testing</span>
+                <Map google={this.props.google} />
+            </div>
+        )
+    }
+}
+
+class FolmarMap extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: defaultLat,
+            lng: defaultLng
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://api.apility.net/geoip/9.9.9.9?token=4c3af26b-44a7-41ee-8505-6b2a9b7e81b8')
+            .then(res => {
+                this.setState({ data: res.data })
+            }) // setState to the returned json data from api call above
+            .catch(err => console.err(err));
+    }
+
+    render() {
+        return <GoogleMap defaultZoom={10} defaultCenter={{ lat: this.state.defaultLat, lng: this.state.defaultLng }} />
+    }
+}
+
+class Map extends React.Component {
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.google !== this.props.google) {
+            this.loadMap();
+        }
+    }
+
+    componentDidMount() {
+        this.loadMap();
+    }
+
+    loadMap() {
+        if (this.props && this.props.google) {
+            // google is available
+            const { google } = this.props;
+            const maps = google.maps;
+
+            const mapRef = this.refs.map;
+            const node = ReactDOM.findDOMNode(mapRef);
+
+            let zoom = 14;
+            let lat = 37.774929;
+            let lng = -122.419416;
+            const center = new maps.LatLng(lat, lng);
+            const mapConfig = Object.assign({}, {
+                center: center,
+                zoom: zoom
+            })
+            this.map = new maps.Map(node, mapConfig);
+        }
+    }
+
+    render() {
+        return (
+            <div ref='map'>
+                Loading map...
+      </div>
+        )
+    }
+}
+
+ReactDOM.render(<FolmarMap />, document.getElementById("root"));
+
+//GoogleApiWrapper({
+//    apiKey: 'AIzaSyCY86Q1_a7yhfX-yWBxsi_rgkKtTsELUGk'
+//})(Container)
